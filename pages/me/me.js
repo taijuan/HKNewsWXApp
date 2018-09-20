81c16a43964206aef6d3a147bba71dfe5925bd56
27e_673636090_zuiweng <27e_673636090_zuiweng@git.cloud.tencent.com>
2018-09-20 18:11:52 +0800

    我的功能添加
    me

--- pages/me/me.js
+++ pages/me/me.js
@@ -5,62 +5,87 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    data: [{
+        icon: "../../images/favorite.png",
+        name: "My Favorites",
+        pagePath: ""
+      },
+      {
+        icon: "../../images/facebook.png",
+        name: "Facebook",
+        pagePath: "../../pages/web/web?url=https://www.facebook.com/chinadailyhkedition/"
+      },
+      {
+        icon: "../../images/twitter.png",
+        name: "Twitter",
+        pagePath: "../../pages/web/web?url=https://twitter.com/chinadailyasia"
+      },
+      {
+        icon: "../../images/feedback.png",
+        name: "Feedback",
+        pagePath: ""
+      },
+      {
+        icon: "../../images/settings.png",
+        name: "Settings",
+        pagePath: ""
+      },
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
